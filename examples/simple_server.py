62b62c327d2cfaf8aa9067bceb9d5d1956bca086
Rafael Passos <rafael@rcpassos.me>
2026-08-17 23:13:39 -0300

    example: rename simple to simple_server
    Signed-off-by: Rafael Passos <rafael@rcpassos.me>

--- examples/simple_server.py
+++ examples/simple_server.py
@@ -1,6 +1,6 @@
 import os
 
-from tinysupervisor import CronJob, Job, Job, Service, init_supervisor
+from tinysupervisor import CronJob, Job, Process, Service, init_supervisor
 
 
 def render_index(file_names: list[str]) -> str:
@@ -36,9 +36,9 @@ def write(file_name: str, content: str):
 
 
 def index_folder_runner(folder_name):
-    files = Job.run(f"ls {folder_name}")
+    files = Process.run(f"ls {folder_name}")
     index = render_index(files)
-    Job.run(write, args=["index.html", index])
+    Process.run(write, args=["index.html", index])
 
 
 def server(folder_name):
@@ -49,38 +49,33 @@ def server(folder_name):
 
 def main():
     supervisor = init_supervisor()
-    # define task dependency each register task should depende of the previous,
-    # they should only need them to start, not complete
-    supervisor.auto_dependency_mode(mode="register_order", wait_for="start")
 
-    supervisor.register(Job(name="init_folder", executable="mkdir output", args=[]))
+    supervisor.register(Job(name="init_folder", command="mkdir -p output"))
     supervisor.register(
         CronJob(
             name="index_folder",
             interval="1m",
             executable=index_folder_runner,
-            args=[],
             kwargs={"folder_name": "output"},
+            depends=["init_folder"],
+            dependency_mode="completed",
         )
     )
-    supervisor.register(server, kwargs={"folder_name": "output"})
+    supervisor.register(
+        Service(
+            name="server",
+            executable=server,
+            kwargs={"folder_name": "output"},
+            depends=["init_folder"],
+            dependency_mode="completed",
+        )
+    )
 
     supervisor.set_heartbeat_interval("1m")  # default reconciler interval
 
     supervisor.set_http_port(
         int(os.environ.get("TINYSUPERVISOR_HTTP_PORT", "8081"))
     )  # default value
-    # will serve these endpoints:
-    # /health
-    # /healthz
-    # -> return status 200 if OK, 400 if any process is not in its desired state
-    #    bory returns state of each process in json:
-    #       {"processes":[
-    #       {"a": {"current": "running", "desired": "running"}},
-    #       {"b": {"current": "sleeping", "desired": "sleeping"}},
-    #       {"c": {"current": "stopped", "desired": "stopped"}},
-    #       ]}
-    #
 
     supervisor.start()
 
